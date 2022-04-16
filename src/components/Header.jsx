6c6c543155fc9f315e25526c6840d0e708d957f2
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2022-04-17 02:08:09 +0300

    coloring header relates #1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Header(props) {
+function Header({ text, bgColor, textColor }) {
+  const headerStyle = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
   return (
-    <header>
+    <header style={headerStyle}>
       <div className="container">
         <h2>Feedback UI</h2>
-        <h3>{props.text}</h3>
       </div>
     </header>
   );
 }
 Header.defaultProps = {
   text: "Hello from Default Props",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
 };
 Header.prototype = {
   text: PropTypes.string,
